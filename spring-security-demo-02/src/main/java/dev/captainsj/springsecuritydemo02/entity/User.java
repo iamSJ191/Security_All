845e785cbfbac1d8d5a79afcdeb9f56c155c297f
CaptainSJ <i.saurabhjoshi@hotmail.com>
2023-03-27 21:21:34 +0530

    updated repo
    Signed-off-by: CaptainSJ <i.saurabhjoshi@hotmail.com>

--- spring-security-demo-02/src/main/java/dev/captainsj/springsecuritydemo02/entity/User.java
+++ spring-security-demo-02/src/main/java/dev/captainsj/springsecuritydemo02/entity/User.java
@@ -10,11 +10,8 @@ import javax.persistence.*;
 @Getter
 @Setter
 public class User {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String username;
-
     private String password;
 }
