6421cc01e3fc762e1c03f9387d6ef430160988b4
CaptainSJ <i.saurabhjoshi@hotmail.com>
2023-04-01 09:44:02 +0530

    minor changes
    Signed-off-by: CaptainSJ <i.saurabhjoshi@hotmail.com>

--- spring-security-demo-02/src/main/java/dev/captainsj/springsecuritydemo02/SpringSecurityDemo02Application.java
+++ spring-security-demo-02/src/main/java/dev/captainsj/springsecuritydemo02/SpringSecurityDemo02Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityDemo02Application {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityDemo02Application.class, args);
     }
-
 }
